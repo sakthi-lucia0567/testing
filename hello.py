2a5408bd4cf58de4dccae2e2186798cd3b7b6f12
sakthi-lucia0567 <sakthivel8725@gmail.com>
2023-03-06 05:38:37 +0530

    ci: Continue Learning Python

--- hello.py
+++ hello.py
@@ -1,1 +1,40 @@
-print("Hello World")
+# print("Hello World")
+
+# this is my first comment
+"""multi line 
+comment"""
+
+# def f1():
+#     global x
+#     x+=1
+#     print(x)
+
+# x = 12
+# f1()
+# print("x")
+
+# def fun():
+#     x = 15
+#     print(x)
+# x = 12
+# fun()
+
+# a = 7
+# b = 7.0
+# c = a + b
+# print(c)
+# print(type(c))
+
+# a = "hello"
+# b = 2
+# c = a * b
+# print(c)
+# print(type(c))
+
+# a = "name"
+# b = int(a)
+# print(type(b))
+
+name = "A"
+d = ord(name)
+print(type(d))
